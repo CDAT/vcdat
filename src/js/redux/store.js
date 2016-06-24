2e32abffb59a9a99adc6ea29a1d6c7f75dd15fc6
Bryce Sampson <sampson.bryce@yahoo.com>
2016-06-24 08:56:14 -0700

    reworked file structure

--- src/js/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux'
-import undoableReducer from './reducer.js'
-
-
-//If you want middleware/logging add it here
-
-export default function configureStore(initialState = {}){
-    return createStore(undoableReducer, initialState);
-}
