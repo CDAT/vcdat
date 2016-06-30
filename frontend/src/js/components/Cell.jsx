d08ea5cfb7d08cd9def93920ac615f226bc43fd3
Bryce Sampson <sampson.bryce@yahoo.com>
2016-06-29 21:18:32 -0700

    basic windowing with undo redo working

--- frontend/src/js/components/Cell.jsx
+++ frontend/src/js/components/Cell.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import ResizeSensor from 'css-element-queries/src/ResizeSensor'
 
 var Cell = React.createClass({
+    componentDidMount(){
+        var element = $('.cell')[0];
+        new ResizeSensor(element, () => {
+            console.log('resized');
+            $('.cell-image').each((index, el) => {
+                el = $(el);
+                el.height(el.parent().height());
+            })
+        })
+    },
     render(){
+        console.log(ResizeSensor);
         return(
-            <img className='cell-image' src='http://www.nasa.gov/sites/default/files/thumbnails/image/15-115.jpg' alt='climate_data'></img>
+            // <div className='cell'></div>
+            <div className='cell'>
+                <img className='cell-image' src='http://www.nasa.gov/sites/default/files/thumbnails/image/15-115.jpg' alt='climate_data'></img>
+            </div>
         )
     }
 });
--- frontend/src/js/components/Toolbar.jsx
+++ frontend/src/js/components/Toolbar.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 var Toolbar = React.createClass({
     render() {
+        console.log('undo redo', this.props.undoEnabled, this.props.redoEnabled);
         return (
             <div className='toolbar'>
-                <button className='undo-redo-button' onClick={this.props.onUndo} disabled={!this.props.undoEnabled}>
+                <button className='btn undo-redo-button' onClick={this.props.onUndo} disabled={!this.props.undoEnabled}>
                     <img src='/img/undo_icon.png'></img>
                 </button>
-                <button className='undo-redo-button' onClick={this.props.onRedo} disabled={!this.props.redoEnabled}>
+                <button className='btn undo-redo-button' onClick={this.props.onRedo} disabled={!this.props.redoEnabled}>
                     <img src='/img/redo_icon.png'></img>
                 </button>
             </div>
