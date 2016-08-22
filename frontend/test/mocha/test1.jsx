608d137bba1f02f0987af625ba60b7b69f73a59b
Ed Brown <ebrown37@mail.csuchico.edu>
2016-08-22 13:23:28 -0700

    Got mocha to run a test for a simple React component.
    Used the following command to run the test:
    ```$ mocha --require test/mocha/dom-mock.js test/mocha/test1.jsx --compilers jsx:babel-core/register --recursive```

--- frontend/test/mocha/test1.jsx
+++ frontend/test/mocha/test1.jsx
@@ -6,7 +6,7 @@ var jsdom = require('mocha-jsdom');
 var assert = require('assert');
 var React = require('react');
 var TestUtils = require('react-addons-test-utils');
-import TestComponent from './TestComponent.jsx'
+var TestComponent = require('./TestComponent.jsx').default;
 
 beforeEach(function(){
     dom_mock('<html><body></body></html>');
@@ -16,7 +16,6 @@ describe('Testing my div', function() {
   jsdom({ skipWindowCheck: false });
 
   it('should contain text: TEST', function() {
-    var TestComponent = require('./TestComponent.jsx');
     var myDiv = TestUtils.renderIntoDocument(
       <TestComponent />
     );
