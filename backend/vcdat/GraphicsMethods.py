196d93f7d39656d169fd31cba106c7d989af7678
Pawlicka Maule <anna.maulle@gmail.com>
2016-08-22 11:57:37 -0700

    taking isofill out of the primitives

--- backend/vcdat/GraphicsMethods.py
+++ backend/vcdat/GraphicsMethods.py
@@ -4,7 +4,7 @@ import json
 _ = vcs.init()
 _methods = {}
 _2d_methods = ('scatter', 'vector', 'xvsy', 'stream', 'glyph', '3d_vector', '3d_dual_scalar')
-_primitives = ('line', 'marker', 'fillarea', 'text','isofill')
+_primitives = ('line', 'marker', 'fillarea', 'text')
 
 
 def get_gm():
