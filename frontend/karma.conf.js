c0fb2e6fb62d906c28d83c568e01a6dc66de3a4b
embrown <brown308@llnl.gov>
2016-09-28 13:01:36 -0700

    Trying to get code coverage working.

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,27 +1,37 @@
 var webpack = require('webpack');
+var webpack_config = require('./webpack.config.js');
 
 module.exports = function (config) {
   config.set({
-    browsers: [ 'Chrome', 'Firefox' ],
-    singleRun: true, // just run once by default
-    frameworks: [ 'mocha' ], // use the mocha test framework
-    files: [
-      'tests.webpack.js' // just load this file
-    ],
-    preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ] // preprocess with webpack and our sourcemap loader
-    },
-    reporters: [ 'dots' ], // report results in this format
-    webpack: { // kind of a copy of your webpack config
-      devtool: 'inline-source-map', // just do inline source maps instead of the default
-      module: {
-        loaders: [
-          { test: /[A-Z]+[a-z]+Test\.js[x]{0,1}$/, loader: 'babel-loader' }
-        ]
+    basePath: "./",
+    browsers: [ 'Chrome', 'Firefox' ], //run in Chrome
+    singleRun: true, //just run once by default
+    frameworks: [ 'mocha', 'chai' ], //use the mocha test framework
+    client: {
+      mocha: {
+        grep: '/.*Test.js[x]{0,1}/',
+        compilers: 'jsx:babel-core/register',
       }
     },
+    files: [
+      'test/mocha/dom-mock.js',
+      'tests.webpack.js' //just load this file
+    ],
+    plugins: [ 'karma-chrome-launcher', 'karma-chai', 'karma-mocha',
+      'karma-sourcemap-loader', 'karma-webpack', 'karma-coverage',
+      'karma-mocha-reporter', 'babel-core', 'babel-loader'
+    ],
+    preprocessors: {
+      'tests.webpack.js': [ 'babel-loader', 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+    },
+    reporters: [ 'mocha', 'coverage' ], //report results in this format
+    webpack: webpack_config,
     webpackServer: {
-      noInfo: true // please don't spam the console when running in karma!
+      noInfo: true //please don't spam the console when running in karma!
+    },
+    coverageReporter: {
+      type: 'html', //produces a html document after code is run
+      dir: 'coverage/' //path to created html doc
     }
   });
 };
