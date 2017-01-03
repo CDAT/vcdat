309f93eb07bcf8f2f810d2a97eea3941b5a5d5a3
Sam Fries <samuelbfries@gmail.com>
2017-01-03 07:45:19 -0800

    Template preview (#89)
    * Added basic infrastructure for template preview system
    
    * Working template preview
    
    * Made preview be a bit smarter and make many fewer requests

--- backend/vcdat/Templates.py
+++ backend/vcdat/Templates.py
@@ -3,7 +3,18 @@ import json
 
 _templates = {}
 
+
 def get_t():
         for tname in vcs.elements['template'].keys():
             _templates[tname] = vcs.utils.dumpToDict(vcs.elements['template'][tname])[0]
         return _templates
+
+
+def templ_from_json(blob):
+    t = vcs.createtemplate()
+    for a in blob:
+        if isinstance(blob[a], dict):
+            attr = getattr(t, a)
+            for k in blob[a]:
+                setattr(attr, k, blob[a][k])
+    return t
--- frontend/src/js/components/TemplateList.jsx
+++ frontend/src/js/components/TemplateList.jsx
@@ -1,29 +1,51 @@
 import React from 'react'
 import AddEditRemoveNav from './AddEditRemoveNav.jsx'
+import TemplateEditor from './modals/TemplateEditor.jsx'
+/* global $ */
+
+var el = null;
 
 var TemplateList = React.createClass({
     propTypes: {
-        templates: React.PropTypes.array
+        templates: React.PropTypes.object
     },
     getInitialState(){
         return {};
     },
+    selectedChild(val) {
+        if (el !== null) {
+            el.removeClass('bg-primary');
+        }
+        el = $(val.currentTarget);
+        while (el.prop("tagName").toLowerCase() !== "li") {
+            el = el.parent();
+        }
+        el.addClass('bg-primary')
+        this.setState({
+            active_template: el.attr("data-name")
+        });
+    },
+    editTemplate() {
+        $('#template-editor').modal('show')
+    },
     render() {
+        let template = this.state.active_template ? this.props.templates[this.state.active_template] : this.props.templates.default;
         return (
             <div className='left-side-list scroll-area-list-parent'>
-                <AddEditRemoveNav title='Templates'/>
+                <AddEditRemoveNav editAction={this.editTemplate} title='Templates'/>
                 <div className='scroll-area'>
                     <ul id='temp-list' className='no-bullets left-list'>
-                        {this.props.templates.map((value, index) => {
+                        {Object.keys(this.props.templates).map((value, index) => {
                             let class_name = 'main-left-list-item draggable-list-item';
                             return (
-                                <li key={value} className={class_name} data-type='template' data-name={value}>
+                                <li key={value} className={class_name} onClick={this.selectedChild} data-type='template' data-name={value}>
                                     <a>{value}</a>
                                 </li>
                             );
                         })}
                     </ul>
                 </div>
+                <TemplateEditor template={template}/>
             </div>
         );
     }
