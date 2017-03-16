442e8790733d2d151d346ac07921d17d3baf9359
Sam Fries <fries2@llnl.gov>
2017-03-16 11:48:44 -0700

    Converted FileExplorer to Tree; quicktree is dead, long live Tree

--- backend/vcdat/app.py
+++ backend/vcdat/app.py
@@ -131,24 +131,7 @@ def get_colormaps():
 @jsonresp
 def get_initial_file_tree():
     start_path = os.path.expanduser('~')
-    dir_list = start_path.split('/')
-    del dir_list[0]
-    dir_list.insert(0, '/')
-    base_files = {}
-    total_path = ''
-    prev_obj = {}
-    for index, directory in enumerate(dir_list):
-        total_path += directory
-        if index:
-            total_path += '/'
-        files = getFilesObject(total_path)
-
-        if index:
-            prev_obj['sub_items'][directory] = files
-        else:
-            base_files = files
-        prev_obj = files
-
+    base_files = getFilesObject(start_path)
     return json.dumps(base_files)
 
 
