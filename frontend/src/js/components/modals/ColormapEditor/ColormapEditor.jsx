75fdc769969dd1d03eb40fadfe3671c2490083ef
Crean <crean2@llnl.gov>
2017-11-06 08:56:27 -0800

    Makes ColormapEditor apply button change based on num of col/rows

--- frontend/src/js/components/modals/ColormapEditor/ColormapEditor.jsx
+++ frontend/src/js/components/modals/ColormapEditor/ColormapEditor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Modal, Button, Dropdown, MenuItem } from 'react-bootstrap';
 
 import ColorPicker from './ColorPicker.jsx'
@@ -18,6 +19,8 @@ class ColormapEditor extends Component {
         return {
             show: React.PropTypes.bool.isRequired, // show the modal
             close: React.PropTypes.func.isRequired, // close the modal
+            sheet_num_cols: React.PropTypes.number,
+            sheet_num_rows: React.PropTypes.number,
         }; 
     }
 
@@ -33,7 +36,59 @@ class ColormapEditor extends Component {
         this.setState({showImportExportModal: true})
     }
 
+    getApplyButton(){
+        if(this.props.sheet_num_rows < 2 && this.props.sheet_num_cols < 2){
+            return(
+                <Button
+                    id="colormap-apply-dropup"
+                    style={{float: "left", marginLeft: "5px"}}
+                    onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 0)}}>
+                    Apply
+                </Button>
+            )
+        }
+        else{
+            let menuItems;
+            if(this.props.sheet_num_rows == 2 && this.props.sheet_num_cols == 2){
+                menuItems = (
+                    <Dropdown.Menu className="colormap-apply-menu">
+                        <MenuItem eventKey="0" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 0)}}>To Top Left</MenuItem>
+                        <MenuItem eventKey="1" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 1)}}>To Top Right</MenuItem>
+                        <MenuItem eventKey="2" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(1, 0)}}>To Bottom Left</MenuItem>
+                        <MenuItem eventKey="3" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(1, 1)}}>To Bottom Right</MenuItem>
+                    </Dropdown.Menu>
+                )
+            }
+            else if(this.props.sheet_num_rows == 2){
+                menuItems = (
+                    <Dropdown.Menu className="colormap-apply-menu">
+                        <MenuItem eventKey="0" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 0)}}>To Top</MenuItem>
+                        <MenuItem eventKey="1" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(1, 0)}}>To Bottom</MenuItem>
+                    </Dropdown.Menu>
+                )
+            }
+            else{
+                menuItems = (
+                    <Dropdown.Menu className="colormap-apply-menu">
+                        <MenuItem eventKey="0" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 0)}}>To Left</MenuItem>
+                        <MenuItem eventKey="1" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 1)}}>To Right</MenuItem>
+                    </Dropdown.Menu>
+                )
+            }
+            return(
+                <Dropdown 
+                    dropup
+                    id="colormap-apply-dropup"
+                    style={{float: "left", marginLeft: "5px"}}>
+                    <Dropdown.Toggle>Apply</Dropdown.Toggle>   
+                    {menuItems}
+                </Dropdown>
+            )
+        }
+    }
+
     render(){
+        let apply = this.getApplyButton();
         return(
             <div>
                 <Modal show={this.props.show} onHide={this.props.close}>
@@ -62,18 +117,7 @@ class ColormapEditor extends Component {
                             onClick={() => {this.refs.widget.getWrappedInstance().resetColormap()}}>
                             Reset
                         </Button>
-                        <Dropdown 
-                            dropup
-                            id="colormap-apply-dropup"
-                            style={{float: "left", marginLeft: "5px"}}>
-                            <Dropdown.Toggle>Apply</Dropdown.Toggle>
-                            <Dropdown.Menu className="colormap-apply-menu">
-                                <MenuItem eventKey="0" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 0)}}>To Top Left</MenuItem>
-                                <MenuItem eventKey="1" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(0, 1)}}>To Top Right</MenuItem>
-                                <MenuItem eventKey="2" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(1, 0)}}>To Bottom Left</MenuItem>
-                                <MenuItem eventKey="3" onClick={() => {this.refs.widget.getWrappedInstance().applyColormap(1, 1)}}>To Bottom Right</MenuItem>
-                            </Dropdown.Menu>
-                        </Dropdown>
+                        {apply}
                         <Button onClick={this.openImportExportModal.bind(this)}>Import/Export</Button>
                         <Button onClick={this.props.close}>Close</Button>
                     </Modal.Footer>
@@ -83,4 +127,11 @@ class ColormapEditor extends Component {
     }
 }
 
-export default ColormapEditor;
+const mapStateToProps = (state) => {
+    return {
+        sheet_num_rows: state.present.sheets_model.sheets[state.present.sheets_model.cur_sheet_index].row_count,
+        sheet_num_cols: state.present.sheets_model.sheets[state.present.sheets_model.cur_sheet_index].col_count,
+    }
+}
+
+export default connect(mapStateToProps)(ColormapEditor);
