2557075badb710af9424ca2ea2231adaae683c27
Crean <crean2@llnl.gov>
2017-11-13 16:06:42 -0800

    Adds redux-test-utils for testing components connected to the redux store. adds example test

--- frontend/test/mocha/components/Modals/ColormapEditor/ColormapEditor.jsx
+++ frontend/test/mocha/components/Modals/ColormapEditor/ColormapEditor.jsx
@@ -5,8 +5,20 @@ var React = require('react');
 
 import ColormapEditor from '../../../../../src/js/components/modals/ColormapEditor/ColormapEditor.jsx'
 import { shallow } from 'enzyme'
+import { createMockStore } from 'redux-test-utils'
 
-const colormap_editor = shallow(<ColormapEditor/>)
+const state = {
+    present: {
+        sheets_model: {
+            sheets: [
+                {row_count: 1, col_count: 1}
+            ],
+            cur_sheet_index: 0
+        }
+    }
+}
+const store = createMockStore(state)
+const colormap_editor = shallow(<ColormapEditor store={store}/>)
 
 describe('ColormapEditorTest.jsx', function() {
     it('renders without exploding', () => {
