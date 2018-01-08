1df0fb0387d1ea28b36ddc34cf9af45de2483733
Crean <crean2@llnl.gov>
2018-01-08 11:04:29 -0800

    Completes testing for TabBar

--- frontend/src/js/components/modals/CachedFiles/Tabs/TabBar.jsx
+++ frontend/src/js/components/modals/CachedFiles/Tabs/TabBar.jsx
@@ -6,11 +6,11 @@ class TabBar extends PureComponent{
     render(){
         return(
             <div className="text-center variable-tabs">
-                <Button bsSize="small" bsStyle={this.props.selectedTab == "file" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('file')}}>File</Button>
-                <Button disabled bsSize="small" bsStyle={this.props.selectedTab == "esfg" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('esfg')}}>ESGF</Button>
-                <Button disabled bsSize="small" bsStyle={this.props.selectedTab == "opendap" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('opendap')}}>OpenDAP</Button>
-                <Button disabled bsSize="small" bsStyle={this.props.selectedTab == "edit" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('edit')}}>Edit</Button>
-                <Button bsSize="small" bsStyle={this.props.selectedTab == "info" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('info')}}>Info</Button>
+                <Button id="tabbar-file" bsSize="small" bsStyle={this.props.selectedTab == "file" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('file')}}>File</Button>
+                <Button id="tabbar-esgf" disabled bsSize="small" bsStyle={this.props.selectedTab == "esfg" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('esgf')}}>ESGF</Button>
+                <Button id="tabbar-opendap" disabled bsSize="small" bsStyle={this.props.selectedTab == "opendap" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('opendap')}}>OpenDAP</Button>
+                <Button id="tabbar-edit" disabled bsSize="small" bsStyle={this.props.selectedTab == "edit" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('edit')}}>Edit</Button>
+                <Button id="tabbar-info" bsSize="small" bsStyle={this.props.selectedTab == "info" ? 'primary' : 'default'} onClick={() => {this.props.switchTab('info')}}>Info</Button>
             </div>
         )
     }
