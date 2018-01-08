dd2351688bc92d09f3c92f57116202a909b8db00
Crean <crean2@llnl.gov>
2018-01-08 14:51:25 -0800

    Adds render test for FileExplorer

---
+++ frontend/test/mocha/components/Modals/FileExplorer/FileExplorerTest.jsx
@@ -0,0 +1,14 @@
+/* globals it, describe, before, beforeEach, */
+var chai = require('chai');
+var expect = chai.expect;
+var React = require('react');
+
+import FileExplorer from '../../../../../src/js/components/modals/FileExplorer/FileExplorer.jsx'
+import { shallow } from 'enzyme'
+
+describe('FileExplorerTest.jsx', function() {
+    it('renders without exploding', () => {
+        const file_explorer = shallow(<FileExplorer/>)
+        expect(file_explorer).to.have.lengthOf(1);
+    });
+});
