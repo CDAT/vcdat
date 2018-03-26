1a132bc4e26b989ea2f91e67aab322d8fc1a2abb
Crean <crean2@llnl.gov>
2018-03-26 16:04:52 -0700

    Begins implementing a general vCDAT tour feature.

--- frontend/src/js/constants/Constants.js
+++ frontend/src/js/constants/Constants.js
@@ -1,5 +1,35 @@
 const ONE_VAR_PLOTS = ["boxfill", "isofill", "isoline"] // meshfill can be either 1 or 2
 const TWO_VAR_PLOTS = ["vector", "3d_vector", "streamline"]
 
+const JOYRIDE_STEPS = [
+    {
+        title: 'Welcome to vCDAT!',
+        text: 'The following tour will help guide you through the basic features of vCDAT. Click "Next" to continue the tour.',
+        selector: '#app',
+        position: 'top',
+        type: 'click',
+        isFixed: true,
+        style: {
+            arrow: {
+                display: 'none'
+            },
+        }   
+    },
+    {
+        title: 'Plot Parts',
+        text: 'There are three parts to any given plot. Variables, a Graphics Method, and a Template. Each of which can be dragged and dropped into a cell.',
+        selector: '#left-side-bar',
+        position: 'right',
+        type: 'click',
+    },
+    {
+        title: 'Variables',
+        text: 'Use the \'Add\' \'Edit\' and \'Delete\' buttons to modify what variables are loaded. In order to edit or delete a variable, click on it to select it first.',
+        selector: '.var-container .navbar-right.side-nav',
+        position: 'right',
+        type: 'click',   
+    }
+]
+export { JOYRIDE_STEPS }
 export { ONE_VAR_PLOTS }
 export { TWO_VAR_PLOTS }
--- frontend/src/js/containers/AppContainer.jsx
+++ frontend/src/js/containers/AppContainer.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TopBar from './TopBar/TopBar.jsx'
 import LeftSideBar from './LeftSideBar.jsx';
-import RightSideBar from './RightSideBar.jsx';
 import SpreadsheetContainer from './SpreadsheetContainer/SpreadsheetContainer.jsx';
 import { ActionCreators as UndoActionCreators } from 'redux-undo';
 import { connect } from 'react-redux';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { ToastContainer } from 'react-toastify';
+import { JOYRIDE_STEPS } from '../constants/Constants.js'
+import Joyride from 'react-joyride'
+import 'react-joyride/lib/react-joyride.scss'
 /* global jQuery */
 
-var AppContainer = React.createClass({
-    propTypes: {
-        undo: React.PropTypes.func,
-        redo: React.PropTypes.func,
-        undoEnabled: React.PropTypes.bool,
-        redoEnabled: React.PropTypes.bool
-    },
+class AppContainer extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            jr_steps: JOYRIDE_STEPS,
+            jr_run: false,
+            jr_auto_start: false,
+        }
+        this.startTour = this.startTour.bind(this)
+    }
+
+    componentDidMount(){
+        
+    }
+
+    startTour(){
+        if(this.joyride){
+            this.setState({jr_run: true})
+        }
+    }
+
     render() {
         return (
             <div id='app-container'>
-                <TopBar onUndo={this.props.undo} onRedo={this.props.redo} undoEnabled={this.props.undoEnabled} redoEnabled={this.props.redoEnabled}/>
+                <Joyride ref={(el)=>{this.joyride = el}}
+                    steps={this.state.jr_steps}
+                    run={this.state.jr_run}
+                    autoStart={true}
+                    type='continuous'
+                />
+                <TopBar
+                    onUndo={this.props.undo}
+                    onRedo={this.props.redo}
+                    undoEnabled={this.props.undoEnabled}
+                    redoEnabled={this.props.redoEnabled}
+                    startTour={this.startTour}
+                />
                 <div id="main-container">
                     <LeftSideBar resizeSpreadsheet={this.resizeSpreadsheet} />
                     <SpreadsheetContainer />
@@ -29,7 +57,14 @@ var AppContainer = React.createClass({
             </div>
         );
     }
-});
+}
+
+AppContainer.propTypes = {
+    undo: React.PropTypes.func,
+    redo: React.PropTypes.func,
+    undoEnabled: React.PropTypes.bool,
+    redoEnabled: React.PropTypes.bool
+}
 
 const mapStateToProps = (state) => {
     var undoEnabled = state.past.length > 0;
