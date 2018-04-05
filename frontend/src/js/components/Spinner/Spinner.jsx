ff1d38cc04c491c0cbc87490beb72d39cb02e621
Crean <crean2@llnl.gov>
2018-04-05 13:49:54 -0700

    Updates Spinner to be class based, updates to prop-types

--- frontend/src/js/components/Spinner/Spinner.jsx
+++ frontend/src/js/components/Spinner/Spinner.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { Button, Glyphicon, FormControl, InputGroup } from 'react-bootstrap';
 import './Spinner.scss';
-/* global $*/
 
-var Spinner = React.createClass({
-    propTypes: {
-        max: React.PropTypes.number,
-        min: React.PropTypes.number,
-        update: React.PropTypes.func,
-        value: React.PropTypes.number,
-        addon_label: React.PropTypes.string,
-    },
+class Spinner extends PureComponent {
+    constructor(props){
+        super(props)
+        this.incrementValue = this.incrementValue.bind(this)
+        this.decrementValue = this.decrementValue.bind(this)
+    }
+
     incrementValue() {
         let value = this.props.value + 1;
         if (value <= this.props.max) {
             this.props.update(value)
         }
-    },
+    }
+
     decrementValue() {
         let value = this.props.value -1 ;
         if (value >= this.props.min) {
             this.props.update(value)
         }
-    },
+    }
+
     handleInputChange(value) {
         let new_value = Math.max(this.props.min, Math.min(this.props.max, value));
         this.props.update(new_value);
-    },
+    }
+
     render() {
         return (
             <InputGroup bsSize="small" className="spinner">
@@ -39,6 +41,14 @@ var Spinner = React.createClass({
             </InputGroup>
         )
     }
-});
+}
+
+Spinner.propTypes = {
+    max: PropTypes.number,
+    min: PropTypes.number,
+    update: PropTypes.func,
+    value: PropTypes.number,
+    addon_label: PropTypes.string,
+}
 
 export default Spinner;
