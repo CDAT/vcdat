7c82adc39475d2046dbb1a09055b6122fbcea1e0
Crean <crean2@llnl.gov>
2018-06-13 15:44:10 -0700

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   packages=['vcdat'],
   package_dir={'vcdat': 'backend/vcdat'},
   package_data={"vcdat": ["resources/" + f for f in os.listdir("backend/vcdat/resources") if f[0] != "."]},
-  scripts=["scripts/vcdat", "scripts/check_vcdat_update.py"]
+  scripts=["scripts/vcdat", "scripts/check_vcdat_update.py"],
   extras_require={
       'test':  ["pytest", "pytest-cov"],
   }
