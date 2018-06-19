6eb614fdd50d7cfef507f66d8db625efa0c59242
Crean <crean2@llnl.gov>
2018-06-18 18:47:38 -0700

    Adds basic selenium test

--- backend/vcdat/test_end_to_end.py
+++ backend/vcdat/test_end_to_end.py
@@ -1,15 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import pytest
-
 
 # Declare Fixtures
 #--------------------------------------------------------------------
-@pytest.fixture(scope="module")
-def selenium(request):
-    
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.close()
 
 
 
 
 # End to End testing with Selenium
 #--------------------------------------------------------------------
+
+@pytest.mark.selenium
+def test_server_running(driver):
+    driver = webdriver.Chrome()
+    driver.get("localhost:5000")
+    assert "vCDAT" in driver.title
+    driver.close()
+
+@pytest.mark.selenium
+def test_variable_loads(driver):
+    
+    driver.get("localhost:5000")
 
