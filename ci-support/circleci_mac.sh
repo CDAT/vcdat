5426b94e0d9434c080f381489e7e57753be99412
Crean <crean2@llnl.gov>
2018-07-25 10:18:55 -0700

    Updates sass package. Adds options to script to reliably catch errors

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -1,6 +1,17 @@
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
 export UVCDAT_ANONYMOUS_LOG=False
 export UVCDAT_SETUP_PATH=${HOME}
 export PATH=${HOME}/miniconda/bin:${PATH}
+
 cd frontend
 npm install
 cd ..
