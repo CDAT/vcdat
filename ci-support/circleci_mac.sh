bd3012731ea202187a16ce5859cadac6a7953620
James Crean <creanjames@gmail.com>
2018-07-25 12:40:36 -0700

    Circleci 2.0 (#315)
    * Fixes ci scripts and updates circleci to 2.0

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -1,6 +1,16 @@
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
 export UVCDAT_ANONYMOUS_LOG=False
-export UVCDAT_SETUP_PATH=${HOME}
 export PATH=${HOME}/miniconda/bin:${PATH}
+
 cd frontend
 npm install
 cd ..
@@ -9,9 +19,9 @@ cd frontend
 npm run coverage
 RESULT=$?
 npm run coveralls
-RESULT=$RESULT + $?
+RESULT=$(($RESULT + $?))
 cd ..
-echo "test commands exit result:",$RESULT
+echo "test commands exit result: $RESULT"
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then conda install -n root conda-build anaconda-client ; fi
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./ci-support/conda_upload.sh ; fi
 exit $RESULT
